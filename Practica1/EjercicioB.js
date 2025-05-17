8b18b426173150b955690147a11ea97b4d800760
Ssasja1 <144088619+Ssasja1@users.noreply.github.com>
2025-05-16 19:54:16 -0600

    Segundo Programa

---
+++ Practica1/EjercicioB.js
@@ -0,0 +1,11 @@
+//Version Antigua
+// function cuadrado(num) {
+//     return num * num;
+// }
+
+//b. Convierte esta función tradicional a una arrow function que haga exactamente lo mismo:
+const cuadrado = num => num * num;
+
+console.log(cuadrado(5)); // 25
+console.log(cuadrado(10)); // 100
+console.log(cuadrado(15)); // 225
