e7f5a9afd0c6395025df9d5779f9b499fa26f648
Ssasja1 <144088619+Ssasja1@users.noreply.github.com>
2025-05-16 19:48:12 -0600

    Primer Programa

---
+++ Practica1/Ejercicio1.js
@@ -0,0 +1,18 @@
+// var nombre = "Armando";
+// var edad = 25;
+
+// nombre = "Ana Maria";
+
+// var saludo = "Hola " + nombre + " tienes " + edad + " años.";
+
+// console.log(saludo);
+
+// Ejercicio a. Corrige el siguiente código para que siga las buenas prácticas de JavaScript
+// moderno (usa let y const en lugar de var) y asegúrate de que las variables no se
+// puedan reasignar si no es necesario.
+
+let nombre = "Armando";
+const edad = 25;
+nombre = "Ana Maria";
+const saludo = "Hola " + nombre + " tienes " + edad + " años.";
+console.log(saludo);
