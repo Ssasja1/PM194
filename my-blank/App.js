a8530d413182fdec561d382cf1539d8c856b877b
Ssasja1 <144088619+Ssasja1@users.noreply.github.com>
2025-06-11 19:08:03 -0600

    Componentes React Native

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,29 @@
+// Zona 1: Importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+const Texto = (props) => {
+  const { contenido } = props;
+  return (
+    <Text> {contenido}</Text>
+  )
+};
+
+// Zona 2: Main - Ejecucion
+//Si se quiere poner info, debe estar dentro de un view
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido ="Hola"></Texto>
+       <Texto contenido ="Mundo"></Texto>
+        <Texto contenido ="React Native"></Texto>
+      <Button title='Haber'></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// Zona 3: Estetica del screen, esto es la version de css de react native
 const styles = StyleSheet.create({
   container: {
     flex: 1,
